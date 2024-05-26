6670c7676639ebca39975e02be0383b084efe690
Yulian <138542238+yuvlian@users.noreply.github.com>
2024-05-26 12:19:14 +0700

    Create tools.rs

---
+++ src/tools.rs
@@ -0,0 +1,19 @@
+use crate::models::base::StarrailInfoParsed;
+use crate::client::MihomoAPI;
+use serde::{Deserialize, Serialize};
+
+pub fn remove_empty_dict() {
+    todo!()
+}
+
+pub fn replace_icon_name_with_url() {
+    todo!()
+}
+
+pub fn remove_duplicate_character() {
+    todo!()
+}
+
+pub fn merge_character_data() {
+    todo!()
+}
